12ca52a13c093bdde30c2f347848c38f13e915e6
ehvzmf <yuna00708@gmail.com>
2024-07-13 15:35:18 +0900

    [feat] Add Page component and style

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,8 +4,21 @@ import './index.css';
 
 const MainPage: React.FC = () => {
     return (
-      <div className="main-container">
-        안녕!
+        <div className="main-page">
+        <header className="header">
+          <div className="logo-container">
+            <img src="/path/to/logo.png" alt="Logo" className="logo" />
+            <h1 className="title">Roo <span className="title-highlight">mie</span></h1>
+          </div>
+          <div className="divider"></div>
+        </header>
+        <div className="buttons-container">
+          <button className="button">#세탁</button>
+          <button className="button">#공구</button>
+        </div>
+        <div className="posts-container">
+          {/* 포스트 내용 */}
+        </div>
       </div>
     );
   };
