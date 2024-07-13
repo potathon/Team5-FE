ffb468629cbfab72cda45b8275d48754a5b8f374
ehvzmf <yuna00708@gmail.com>
2024-07-13 15:27:24 +0900

    [feat] Add MainPage

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const MainPage: React.FC = () => {
+    return (
+      <div className="main-container">
+        안녕!
+      </div>
+    );
+  };
+  
+  export default MainPage;
